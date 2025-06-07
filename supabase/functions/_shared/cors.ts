a013f2e0821d13e67cc1b7701dadb4adc5f63659
一丝不苟的沙漠一只雕 <3381466262@qq.com>
2025-06-07 11:14:17 +0800

    Updated cors.ts

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,7 @@
+//
+// 请将这段代码放置在 project/supabase/functions/_shared/cors.ts
+//
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+} 
